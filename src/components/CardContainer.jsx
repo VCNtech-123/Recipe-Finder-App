f2cf92d848a632e30f010426b1485bfcf887e942
John Francis Vecina <johnvecina640@gmail.com>
2026-04-06 22:56:56 +0800

    feat: update Card and CardInfo components to use dynamic data for food details and improve layout

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import TestPicture from '../assets/TestPicture.jpg'
 import { RxCross2 } from "react-icons/rx";
 import { CiHeart } from "react-icons/ci";
 
@@ -26,31 +25,28 @@ class CardInfo extends React.Component {
             <div className="fixed inset-0 bg-black/20 backdrop-blur-sm z-40" onClick={this.props.onClick}></div>
             <div className="flex flex-col fixed bg-background w-5/6 h-4/5 overflow-y-auto top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 rounded-xl" onClick={(e) => e.stopPropagation()}>
                 <div className="w-full h-70 relative">
-                    <img src={TestPicture} alt="" className="w-full max-h-70 object-cover" style={{ 
+                    <img src={picture} alt="" className="w-full max-h-70 object-cover" style={{ 
                         maskImage: 'linear-gradient(to bottom, black 80%, transparent 100%)',
                         WebkitMaskImage: 'linear-gradient(to bottom, black 80%, transparent 100%)' 
                         }}/>
-                    <h1 className="absolute top-45 left-5 font-body text-2xl text-white font-bold">Tinola</h1>
+                    <h1 className="absolute top-45 left-5 font-body text-2xl text-white font-bold">{foodName}</h1>
                     <div className="flex gap-4 absolute top-55 left-4">
-                            <FoodLabel label={'Filipino'} className={'text-xs bg-background/50 flex items-center justify-center py-1 px-5 rounded-2xl text-foreground border-1 border-card '} />
-                            <FoodLabel label={'Stew'} className={'text-xs bg-background/50 flex items-center justify-center py-1 px-5 rounded-2xl text-foreground border-1 border-card '} />
+                            {labels.map(label => <FoodLabel label={label} key={label} className={'text-xs bg-background/50 flex items-center justify-center py-1 px-5 rounded-2xl text-foreground border-1 border-card '} />)}        
                     </div>
                 </div>
                 <div className="flex flex-col px-4 py-3 gap-6">
                     <div className="flex flex-col gap-3">
                         <h1 className="border-l-3 border-l-primary font-bold px-2">Ingredients</h1>
                         <ul className="flex flex-col gap-2 list-disc list-inside marker:text-primary">
-                            <li className="text-sm bg-card p-2 rounded-xl text-muted-foreground"><span className="font-bold text-foreground">Vegetable Oil </span >- 2tbsp</li>
-                            <li className="text-sm bg-card p-2 rounded-xl"><span className="font-bold">Vegetable Oil </span>- 2tbsp</li>
-                            
+                            {ingredients.map(({ ingredient, measurement}) => <Ingredient key={ingredient} ingredient={ingredient} measurement={measurement} />)}
                         </ul>
                     </div>
                     <div className="flex flex-col gap-3">
                          <h1 className="border-l-3 border-l-primary font-bold px-2">Instructions</h1>
-                            <p className="text-xs bg-card p-4 whitespace-pre-line">{`DIRECTIONS:\r\nSTEP 1 - SAUCE\r\nIn a bowl, add 2 Cups of water, 2 Tablespoon soy sauce, 2 Tablespoon white vinegar, sherry cooking wine, 1/4 Teaspoon white pepper, minced ginger, minced garlic, hot pepper, ketchup, hoisin sauce, and sugar.\r\nMix together well and set aside.\r\nSTEP 2 - MARINATING THE CHICKEN\r\nIn a bowl, add the chicken, 1 pinch of salt, 1 pinch of white pepper, 2 egg whites, and 3 Tablespoon of corn starch\r\nSTEP 3 - DEEP FRY THE CHICKEN\r\nDeep fry the chicken at 350 degrees for 3-4 minutes or until it is golden brown and loosen up the chicken so that they don't stick together.\r\nSet the chicken aside.\r\nSTEP 4 - STIR FRY\r\nAdd the sauce to the wok and then the broccoli and wait until it is boiling.\r\nTo thicken the sauce, whisk together 2 Tablespoon of cornstarch and 4 Tablespoon of water in a bowl and slowly add to your stir-fry until it's the right thickness.\r\nNext add in the chicken and stir-fry for a minute and serve on a plate`}</p>
+                            <p className="text-xs bg-card p-4 whitespace-pre-line">{instructions}</p>
                     </div>
                 </div>
-                <div className="absolute bg-white/80 border-white rounded-full p-2 top-2 left-67" onClick={this.props.onClick}>
+                <div className="absolute bg-white/80 border-white rounded-full p-2 top-2 right-2" onClick={this.props.onClick}>
                     <RxCross2 size={20} />
                 </div>
             </div>
@@ -73,29 +69,41 @@ class Card extends React.Component {
 
     render () {
 
+        const { picture, labels, foodName, ingredients, instructions } = this.props
+
         return(
         !this.state.clicked? (
             <div className="flex flex-col w-[calc(50%_-_1rem)] bg-card font-body rounded-xl pb-4 relative" onClick={this.handleClick}>
                 <div>
-                    <img src={TestPicture} alt="" className="rounded-t-xl hover:scale-110 ease-in-out duration-300 transition-transform" />
+                    <img src={picture} alt={'picture'} className="rounded-t-xl hover:scale-110 ease-in-out duration-300 transition-transform" />
                 </div>
                 <div className="px-3">
-                    <h1 className="text-sm my-4 font-bold">Tinola</h1>
+                    <h1 className="text-sm my-4 font-bold">{foodName}</h1>
                     <div className="flex gap-2">
-                        <FoodLabel label={'Filipino'} className={'text-xs bg-background flex items-center justify-center py-1 px-3 rounded-2xl text-foreground border-1 border-card'} />
-                        <FoodLabel label={'Stew'} className={'text-xs bg-background flex items-center justify-center py-1 px-3 rounded-2xl text-foreground border-1 border-card'} />
+                        {labels.map(label => <FoodLabel key={label} label={label} className={'text-xs bg-background flex items-center justify-center py-1 px-3 rounded-2xl text-foreground border-1 border-card'} />)}
                     </div>
                 </div>
-                <div className="absolute bg-white/80 border-white rounded-full p-2 top-2 left-29">
+                <div className="absolute bg-white/80 border-white rounded-full p-2 top-2 right-2">
                     <CiHeart />
                 </div>
             </div>
         ) : (
-            <CardInfo onClick={this.handleClick} onBlur={this.handleClick} />
+            <CardInfo onClick={this.handleClick} ingredients={ingredients} instructions={instructions} foodName={foodName} picture={picture} labels={labels}/>
         )
     )
     }
 
 }
 
-export default Card;
+class CardCointainer extends React.Component {
+        
+    render() {
+         return (
+            <div className="flex flex-wrap gap-4">
+                {this.props.foodList.map(food => <Card key={food.idMeal} picture={food.strMealThumb} labels={food.strTags} foodName={food.strMeal} instructions={food.strInstructions} ingredients={food.ingredients} />)}
+            </div>
+        )
+    }
+}
+
+export default CardCointainer;
